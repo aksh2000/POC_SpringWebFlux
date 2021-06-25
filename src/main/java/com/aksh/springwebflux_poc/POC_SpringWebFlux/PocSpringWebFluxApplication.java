a5df22e75b41b8c035efce55dce3b7575ece191c
Akshith Chittiveli <akshith.varma@quinbay.com>
2021-06-25 22:46:13 +0530

    first commit

---
+++ src/main/java/com/aksh/springwebflux_poc/POC_SpringWebFlux/PocSpringWebFluxApplication.java
@@ -0,0 +1,13 @@
+package com.aksh.springwebflux_poc.POC_SpringWebFlux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocSpringWebFluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocSpringWebFluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aksh/springwebflux_poc/POC_SpringWebFlux/PocSpringWebFluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aksh.springwebflux_poc.POC_SpringWebFlux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocSpringWebFluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
