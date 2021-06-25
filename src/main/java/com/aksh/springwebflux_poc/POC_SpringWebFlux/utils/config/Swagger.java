743589a5fa417d8d6d797645d2b3b40f0e56ade8
Akshith Chittiveli <akshith.varma@quinbay.com>
2021-06-25 23:15:51 +0530

    adds swagger configuration

---
+++ src/main/java/com/aksh/springwebflux_poc/POC_SpringWebFlux/utils/config/Swagger.java
@@ -0,0 +1,29 @@
+package com.aksh.springwebflux_poc.POC_SpringWebFlux.utils.config;
+
+import java.util.Collections;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class Swagger {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class)).paths(PathSelectors.any())
+                .build().securitySchemes(Collections.singletonList(apiKey()));
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("Authorization", "Authorization", "header");
+    }
+}
