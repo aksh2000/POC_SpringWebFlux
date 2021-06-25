74f85ce8fda215150bc3dd017c3a3664583317c8
Akshith Chittiveli <akshith.varma@quinbay.com>
2021-06-25 23:13:08 +0530

    reformats BeanMapper.java

--- src/main/java/com/aksh/springwebflux_poc/POC_SpringWebFlux/utils/helpers/BeanMapper.java
+++ src/main/java/com/aksh/springwebflux_poc/POC_SpringWebFlux/utils/helpers/BeanMapper.java
@@ -7,14 +7,14 @@ import ma.glasnost.orika.MapperFactory;
 import ma.glasnost.orika.impl.DefaultMapperFactory;
 
 public class BeanMapper {
-  private static final MapperFactory mapperFactory = new DefaultMapperFactory.Builder().build();
-  private static final MapperFacade mapper = mapperFactory.getMapperFacade();
+    private static final MapperFactory mapperFactory = new DefaultMapperFactory.Builder().build();
+    private static final MapperFacade mapper = mapperFactory.getMapperFacade();
 
-  public static <S, C> C map(S source, Class<C> clazz) {
-    return source == null ? null : mapper.map(source, clazz);
-  }
+    public static <S, C> C map(S source, Class<C> clazz) {
+        return source == null ? null : mapper.map(source, clazz);
+    }
 
-  public static <S, C> List<C> mapAsList(Iterable<S> source, Class<C> clazz) {
-    return source == null ? null : mapper.mapAsList(source, clazz);
-  }
+    public static <S, C> List<C> mapAsList(Iterable<S> source, Class<C> clazz) {
+        return source == null ? null : mapper.mapAsList(source, clazz);
+    }
 }
